91c75d959045275dca78871ca573d5dd3bf038d6
Lafko <bylafko@gmail.com>
2026-05-19 23:50:39 +0300

    fix for Wraedar

--- GameHelper/Plugin/PluginAssemblyLoadContext.cs
+++ GameHelper/Plugin/PluginAssemblyLoadContext.cs
@@ -1,10 +1,20 @@
 ﻿namespace GameHelper.Plugin
 {
+    using System;
+    using System.Collections.Generic;
     using System.Reflection;
     using System.Runtime.Loader;
+    using GameOffsets;
 
     internal class PluginAssemblyLoadContext : AssemblyLoadContext
     {
+        private static readonly IReadOnlyDictionary<string, Assembly> SharedAssemblies =
+            new Dictionary<string, Assembly>(StringComparer.OrdinalIgnoreCase)
+            {
+                [typeof(IPCore).Assembly.GetName().Name!] = typeof(IPCore).Assembly,
+                [typeof(GameProcessDetails).Assembly.GetName().Name!] = typeof(GameProcessDetails).Assembly,
+            };
+
         private readonly AssemblyDependencyResolver resolver;
 
         public PluginAssemblyLoadContext(string assemblyLocation)
@@ -15,6 +25,12 @@
 
         protected override Assembly? Load(AssemblyName assemblyName)
         {
+            if (assemblyName.Name != null &&
+                SharedAssemblies.TryGetValue(assemblyName.Name, out var sharedAssembly))
+            {
+                return sharedAssembly;
+            }
+
             var path = this.resolver.ResolveAssemblyToPath(assemblyName);
             if (path != null)
             {
